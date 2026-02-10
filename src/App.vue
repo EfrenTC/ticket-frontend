ec6ef2b394231f98753c7a680069a4ad91af9c0d
Efren TC <efrentomascampa@gmail.com>
2026-02-10 14:16:23 +0100

    feat: add axios for API requests and update App.vue to display project status

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref } from 'vue';
+import api from './services/api';
+
+const mensaje = ref('Cargando conexión...');
+
+onMounted(async () => {
+    try {
+        const response = await api.get('/check');
+        mensaje.value = response.data.status;
+    } catch (error) {
+        mensaje.value = 'Error: No se pudo conectar con el servidor';
+        console.error(error);
+    }
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="test-container">
+    <h1>Estado del Proyecto: {{ mensaje }}</h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
